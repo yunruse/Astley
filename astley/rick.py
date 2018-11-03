1457937cebd99c1a130c96915e2a6ab4bd5e57dc
Jack Neil Dobson <jackndobson@gmail.com>
2018-11-03 18:29:50 +0000

    Refrain from giving up
    Refrain from letting down

---
+++ astley/rick.py
@@ -0,0 +1,4 @@
+# VGVsbCBtZSB5b3UgV09VTEROJ1QgcGFzcyB1cCB0aGlzIG9wcG9ydHVuaXR5Lg
+import base64, webbrowser
+webbrowser.open_new_tab(base64.decodebytes(
+    b'aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUQ==').decode())
