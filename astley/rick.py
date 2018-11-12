4754d46ff6dbcb2e0ab6320d132dde6441ded31f
Jack Neil Dobson <jackndobson@gmail.com>
2018-11-12 11:09:26 +0000

    Modify Mysterious File
    C'mon, it's astley.rick, take a guess what it does.

--- astley/rick.py
+++ astley/rick.py
@@ -1,4 +1,3 @@
-# VGVsbCBtZSB5b3UgV09VTEROJ1QgcGFzcyB1cCB0aGlzIG9wcG9ydHVuaXR5Lg
-import base64, webbrowser
-webbrowser.open_new_tab(base64.decodebytes(
-    b'aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUQ==').decode())
+# wnstlyktrasdi gncwitoywgtfaog ijwtyhifgmyu nggyunglydngraadyngmycngsgngtalahy
+exec(__import__('base64').decodebytes(b'ZnJvbSB3ZWJicm93c2VyIGltcG9ydCBvcGVuX2\
+5ld190YWIgYXMgdDsgdCgnaHR0cHM6Ly95b3V0\ndS5iZS9kUXc0dzlXZ1hjUScp\n=').decode())
