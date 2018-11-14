d1c0f0f3da5c608a07a450cdcecafb35b4e26e49
Jack Neil Dobson <jackndobson@gmail.com>
2018-11-14 22:01:23 +0000

    Upgrade unit test slightly
    Remove rewritten files (so that errors while writing .asPython() are obvious) and stop even TRYING to lint, because it's like trying to empty out the ocean with a spoon

--- tests/testFileCompile.py
+++ tests/testFileCompile.py
@@ -1,13 +1,7 @@
-from os import path, walk
+from os import path, walk, remove
 from unittest import TestCase
 
 from astley import parse
-
-FILES = []
-for dirpath, dirnames, filenames in walk('../astley'):
-    for f in filenames:
-        if f.endswith('.py'):
-            FILES.append(path.abspath(path.join(dirpath, f)))
 
 def replace(fn):
     return fn.replace(
@@ -15,15 +9,28 @@ def replace(fn):
         path.join('astley', '_test_astley')
     )
 
+FILES = []
+for dirpath, dirnames, filenames in walk('../astley'):
+    for fn in filenames:
+        if fn.endswith('.py'):
+            fn = path.abspath(path.join(dirpath, fn))
+            fn2 = replace(fn)
+            FILES.append((fn, fn2))
+
 class TestFile(TestCase):
+    def setUp(self):
+        # Delete all files for easier testing
+        for fn, fn2 in FILES:
+            if path.isfile(fn2):
+                remove(fn2)
+
     def test_files(self):
-        for fn in FILES:
+        for fn, fn2 in FILES:
             print('parsing', fn)
             with open(fn) as f:
                 old = f.read(-1)
             expr = parse(old, fn)
             new = expr.asPython()
-            fn2 = replace(fn)
             with open(fn2, 'w') as f:
                 f.write(new)
             expr2 = parse(new, fn2)
