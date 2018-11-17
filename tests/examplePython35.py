fd001cd0963dc632e2b70707e28577bbdabc2ffd
Jack Neil Dobson <jackndobson@gmail.com>
2018-11-17 15:01:06 +0000

    Create test example
    Asyncs are avoided and annotations will be moved to a separate test, when I get around to supporting older versions.

---
+++ tests/examplePython35.py
@@ -0,0 +1,44 @@
+import os
+from testFileCompile import os
+from ..local import eighteen as e
+class Street: ...
+class Äpfelstraße(Street, α=β, γ=lambda x: x&x|x^x):
+    hi = [a for a in b'abc']
+    a = hi[1] = 12 if isinstance(f'A{1:.0f}') else None
+    @staticmethod
+    def function():
+        """Docstring"""
+    def function2(self, a, *args, key: set = {1, 2}, **kwarg):
+        raise ValueError('This is test syntax')
+    def function3(self, *, a=(g for g in 'genexp' if g), **kw):
+        global Street
+        with open(__file__) as f:
+            for i in f.readlines():
+                if 'FOUND' + 'MYSELF' in i:
+                    print('FOUNDMYSELF!')
+                    break
+                elif False:
+                    ...
+                else:
+                    try:
+                        continue
+                    except ValueError as e:
+                        del e
+                    except SyntaxError:
+                        pass
+                    except:
+                        [[], [][1:], ()[:-1], ({}, ())]
+                        [{(): ()}][::-1]
+                        {a for a in 'abc'}
+                        {a: a*2 for a in range(12)}
+                    else:
+                        nonlocal a
+                    finally:
+                        assert True
+
+def naturals(ylim=1, x=0):
+    y : int = ylim
+    while y != 0:
+        y -= 1 - 1 + 1 * 1 / 1 // 1 ** (1 % 1)
+        yield x, y
+    yield from naturals(ylim-1, x+1)
